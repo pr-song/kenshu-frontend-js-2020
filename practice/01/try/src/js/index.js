f5f2ab641c7b1cdf450526ffb18e16148f848700
pr-song <songtranvan2511@gmail.com>
2020-10-17 14:22:55 +0900

    merge objects and print in console

---
+++ practice/01/try/src/js/index.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    const company = {
+        company_id: 1234,
+        company_name: '会社名'
+    };
+
+    const article = {
+        article_id: 9876,
+        article_title: 'タイトル'
+    };
+
+    console.log(company);
+    console.log(article);
+
+    const mergedObjects = { ...company, ...article };
+
+    console.log(mergedObjects);
+})();
